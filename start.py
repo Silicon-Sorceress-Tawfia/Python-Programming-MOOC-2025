5228799e0ce6a5c91e6054efb9470e3e816502f0
Tawfia-Yeasmin <csworktawfia@gmail.com>
2025-03-07 09:31:10 +0600

    Add Python code for MOOC 2025

---
+++ start.py
@@ -0,0 +1,12 @@
+'''Let's begin programming by getting familiar with the print command, 
+which prints text. In this context, 
+printing essentially means that the program will show some text on the screen.
+
+The following program will print the line "Hi there!":'''
+
+print("Hi there!");
+
+'''The program will not work unless the code is written exactly as it is above.
+For example, trying to run the print command without the quotation marks, like so
+
+print(Hi there!)'''
